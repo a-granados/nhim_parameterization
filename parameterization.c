fc91cc8c2b76ea5a329adbb9189f46115f77e023
a-granados <algr@dtu.dk>
2017-02-14 15:41:42 +0100

    param beta added

--- parameterization.c
+++ parameterization.c
@@ -25,6 +25,7 @@ double tXi=5e-2;//Piezoelectric auto-coupling (scaling parameter)
 double tF=1.;//Amplitud of the forcing (scaling parameter)
 double tk=1.;//Piezoelectric  coupling (scaling parameter)
 double tK=8e-2;//Spring constant (scaling parameter) (Hamiltonian coupling)
+double beta=1.; //To introduce differences between the coupled oscillators
 double pi=4.*atan(1.);
 double omega=2.1;//*2/3;
 double lambda=2e-1;
