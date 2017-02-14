2453e2e85294662741ecea745535de9487af14f6
a-granados <algr@dtu.dk>
2017-02-14 17:05:37 +0100

    Restructuring files

--- parameterization.c
+++ parameterization.c
@@ -19,33 +19,6 @@ using std::cout;
 using std::endl;
 //using std::setw;
 
-//System parameters: they are model-specific
-double tzeta=6e-5;
-double tXi=5e-2;//Piezoelectric auto-coupling (scaling parameter)
-double tF=1.;//Amplitud of the forcing (scaling parameter)
-double tk=1.;//Piezoelectric  coupling (scaling parameter)
-double tK=8e-2;//Spring constant (scaling parameter) (Hamiltonian coupling)
-double beta=1.; //To introduce differences between the coupled oscillators
-double pi=4.*atan(1.);
-double omega=2.1;//*2/3;
-double lambda=2e-1;
-double eps=0;
-
-double hini=0.05;
-double hmin=1.0E-5;
-double hmax=1.0;
-double tol=1.0e-13;
-
-//Gridsize:
-size_t Ntheta=200;
-size_t Nc=100;
-double cini=0.1;
-double cend=1.4;
-double csimini=0.2;
-double csimend=1.2;
-double thetasimini=0;
-double thetasimend=1;
-
 int count;
 
 const gsl_interp2d_type *T=gsl_interp2d_bicubic;
@@ -53,6 +26,7 @@ const gsl_interp2d_type *T=gsl_interp2d_bicubic;
 #pragma omp threadprivate(tol)
 
 
+#include "system_parameters.c"
 #include "../piezo_library.c"
 
 using namespace std;
---
+++ system_parameters.c
@@ -0,0 +1,32 @@
+//System parameters: they are model-specific
+double tzeta=6e-5;
+//double tzeta=0.;
+//double tzeta=1e-2;
+//double tXi=0.1;
+//double tXi=5e-1;
+double tXi=5e-2;//Piezoelectric auto-coupling (scaling parameter)
+//double tXi=1e-4;
+//double tXi=0;
+double tF=1.;//Amplitud of the forcing (scaling parameter)
+double tk=1.;//Piezoelectric  coupling (scaling parameter)
+double tK=8e-2;//Spring constant (scaling parameter) (Hamiltonian coupling)
+double beta=1.; //To introduce differences between the coupled oscillators
+double pi=4.*atan(1.);
+double omega=2.1;//*2/3;
+double lambda=2e-1;
+double eps=0;
+
+double hini=0.05;
+double hmin=1.0E-5;
+double hmax=1.0;
+double tol=1.0e-13;
+
+//Gridsize:
+size_t Ntheta=200;
+size_t Nc=100;
+double cini=0.1;
+double cend=1.4;
+double csimini=0.2;
+double csimend=1.2;
+double thetasimini=0;
+double thetasimend=1;
